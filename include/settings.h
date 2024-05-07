2da622f6d22e20a6c96bfc40a686189f41b93021
Andre <supius@gmail.com>
2024-05-07 14:59:07 +0200

    adjust relay levels

--- include/settings.h
+++ include/settings.h
@@ -3,8 +3,8 @@
 // #define RELAY_OFF LOW
 // #define RELAY_ON HIGH
 // ESP-01 mode
-#define RELAY_OFF HIGH
-#define RELAY_ON LOW
+#define RELAY_OFF LOW
+#define RELAY_ON HIGH
 #define LED_OFF HIGH
 #define LED_ON LOW
 #define RELAY_PIN 4
